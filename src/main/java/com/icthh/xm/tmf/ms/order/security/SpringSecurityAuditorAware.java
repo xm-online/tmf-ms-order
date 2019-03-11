5b8910642f22bb38ebead7f909762d6e1b1aed45
dtsarenko <dimatsarenko89@gmail.com>
2019-03-11 17:16:50 +0200

    org import

--- src/main/java/com/icthh/xm/tmf/ms/order/security/SpringSecurityAuditorAware.java
+++ src/main/java/com/icthh/xm/tmf/ms/order/security/SpringSecurityAuditorAware.java
@@ -1,9 +1,7 @@
 package com.icthh.xm.tmf.ms.order.security;
 
 import com.icthh.xm.tmf.ms.order.config.Constants;
-
 import java.util.Optional;
-
 import org.springframework.data.domain.AuditorAware;
 import org.springframework.stereotype.Component;
 
